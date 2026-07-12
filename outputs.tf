5a7365e31aa5489d5953e125678325f3de02c723
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:48 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,53 +1,49 @@
-output "data_factory_dataset_binaries" {
-  description = "All data_factory_dataset_binary resources"
-  value       = azurerm_data_factory_dataset_binary.data_factory_dataset_binaries
-}
 output "data_factory_dataset_binaries_additional_properties" {
-  description = "List of additional_properties values across all data_factory_dataset_binaries"
-  value       = [for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : v.additional_properties]
+  description = "Map of additional_properties values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.additional_properties }
 }
 output "data_factory_dataset_binaries_annotations" {
-  description = "List of annotations values across all data_factory_dataset_binaries"
-  value       = [for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : v.annotations]
+  description = "Map of annotations values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.annotations }
 }
 output "data_factory_dataset_binaries_azure_blob_storage_location" {
-  description = "List of azure_blob_storage_location values across all data_factory_dataset_binaries"
-  value       = [for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : v.azure_blob_storage_location]
+  description = "Map of azure_blob_storage_location values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.azure_blob_storage_location }
 }
 output "data_factory_dataset_binaries_compression" {
-  description = "List of compression values across all data_factory_dataset_binaries"
-  value       = [for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : v.compression]
+  description = "Map of compression values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.compression }
 }
 output "data_factory_dataset_binaries_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_dataset_binaries"
-  value       = [for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.data_factory_id }
 }
 output "data_factory_dataset_binaries_description" {
-  description = "List of description values across all data_factory_dataset_binaries"
-  value       = [for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : v.description]
+  description = "Map of description values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.description }
 }
 output "data_factory_dataset_binaries_folder" {
-  description = "List of folder values across all data_factory_dataset_binaries"
-  value       = [for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : v.folder]
+  description = "Map of folder values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.folder }
 }
 output "data_factory_dataset_binaries_http_server_location" {
-  description = "List of http_server_location values across all data_factory_dataset_binaries"
-  value       = [for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : v.http_server_location]
+  description = "Map of http_server_location values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.http_server_location }
 }
 output "data_factory_dataset_binaries_linked_service_name" {
-  description = "List of linked_service_name values across all data_factory_dataset_binaries"
-  value       = [for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : v.linked_service_name]
+  description = "Map of linked_service_name values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.linked_service_name }
 }
 output "data_factory_dataset_binaries_name" {
-  description = "List of name values across all data_factory_dataset_binaries"
-  value       = [for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : v.name]
+  description = "Map of name values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.name }
 }
 output "data_factory_dataset_binaries_parameters" {
-  description = "List of parameters values across all data_factory_dataset_binaries"
-  value       = [for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : v.parameters]
+  description = "Map of parameters values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.parameters }
 }
 output "data_factory_dataset_binaries_sftp_server_location" {
-  description = "List of sftp_server_location values across all data_factory_dataset_binaries"
-  value       = [for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : v.sftp_server_location]
+  description = "Map of sftp_server_location values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.sftp_server_location }
 }
 
