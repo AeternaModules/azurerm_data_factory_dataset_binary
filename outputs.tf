c86da831407246475027091618c1fb20baf43d4d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:26 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "data_factory_dataset_binaries_id" {
+  description = "Map of id values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.id }
+}
 output "data_factory_dataset_binaries_additional_properties" {
   description = "Map of additional_properties values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
   value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.additional_properties }
