19f92fb4ffed3d816a2ef7af8af9c6ad02393b74
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:14:43 +0100

    Update to schema version v5.0.0

--- outputs.tf
+++ outputs.tf
@@ -12,11 +12,11 @@ output "data_factory_dataset_binaries_annotations" {
 }
 output "data_factory_dataset_binaries_azure_blob_storage_location" {
   description = "Map of azure_blob_storage_location values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
-  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.azure_blob_storage_location if v.azure_blob_storage_location != null && length(v.azure_blob_storage_location) > 0 }
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => one(v.azure_blob_storage_location) if v.azure_blob_storage_location != null && length(v.azure_blob_storage_location) > 0 }
 }
 output "data_factory_dataset_binaries_compression" {
   description = "Map of compression values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
-  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.compression if v.compression != null && length(v.compression) > 0 }
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => one(v.compression) if v.compression != null && length(v.compression) > 0 }
 }
 output "data_factory_dataset_binaries_data_factory_id" {
   description = "Map of data_factory_id values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
@@ -32,7 +32,7 @@ output "data_factory_dataset_binaries_folder" {
 }
 output "data_factory_dataset_binaries_http_server_location" {
   description = "Map of http_server_location values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
-  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.http_server_location if v.http_server_location != null && length(v.http_server_location) > 0 }
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => one(v.http_server_location) if v.http_server_location != null && length(v.http_server_location) > 0 }
 }
 output "data_factory_dataset_binaries_linked_service_name" {
   description = "Map of linked_service_name values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
@@ -48,6 +48,6 @@ output "data_factory_dataset_binaries_parameters" {
 }
 output "data_factory_dataset_binaries_sftp_server_location" {
   description = "Map of sftp_server_location values across all data_factory_dataset_binaries, keyed the same as var.data_factory_dataset_binaries"
-  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => v.sftp_server_location if v.sftp_server_location != null && length(v.sftp_server_location) > 0 }
+  value       = { for k, v in azurerm_data_factory_dataset_binary.data_factory_dataset_binaries : k => one(v.sftp_server_location) if v.sftp_server_location != null && length(v.sftp_server_location) > 0 }
 }
 
